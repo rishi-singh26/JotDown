6bfe9791e4b299d924f4aee101b7bbb9a8495d77
rishi-singh26 <rishisingh0831@gmail.com>
2025-06-19 21:26:36 +0530

    Initial Commit

---
+++ JotDown/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  JotDown
+//
+//  Created by Rishi Singh on 19/06/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
